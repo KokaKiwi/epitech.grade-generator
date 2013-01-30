f7712ec6db73e521f573b78fb7679181c3c38ee7
Guillaume H <hervie_g@hervieg-pc.(none)>
2013-01-30 17:43:03 +0100

    Get ALL the matieres.

--- app.py
+++ app.py
@@ -3,11 +3,22 @@ from flask import Flask
 app = Flask(__name__)
 
 grades = ['A', 'B', 'C', 'D']
+matieres = [
+    'B1 - Anglais',
+    'B1 - C - Prog Elem',
+    'B1 - Mathematiques',
+    'B1 - Igraph',
+    'B1 - Expression Ecrite',
+    'B1 - Systeme Unix'
+]
 
 @app.route("/")
 def hello():
-    grade = random.choice(grades)
-    return 'Ton grade: %s' % (grade)
+    s = 'Tes grades:\n'
+    for matiere in matieres:
+        grade = random.choice(grades)
+        s += '%s : %s\n' % (matiere, grade)
+    return s
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
