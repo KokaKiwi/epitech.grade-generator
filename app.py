f5cbfb61560201da536b180bcd7f6dac711081f1
KokaKiwi <admin@kokabsolu.com>
2013-02-04 17:57:30 +0100

    Added "Echec" and "B1 - Coaching"

--- app.py
+++ app.py
@@ -2,15 +2,16 @@ import os, random
 from flask import Flask
 app = Flask(__name__)
 
-grades = ['A', 'B', 'C', 'D']
+grades = ['A', 'B', 'C', 'D', 'Echec']
 matieres = [
     'B1 - Anglais',
     'B1 - C - Prog Elem',
+    'B1&2 - Culture Informatique',
+    'B1 - Coaching',
     'B1 - Mathematiques',
     'B1 - Igraph',
     'B1 - Expression Ecrite',
     'B1 - Systeme Unix',
-    'B1&2 - Culture Informatique',
 ]
 
 @app.route("/")
